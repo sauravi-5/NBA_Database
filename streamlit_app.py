a2220ba3f39c9654f1888be152648415a60016d2
sauravi-5 <92053483+sauravi-5@users.noreply.github.com>
2024-11-16 17:42:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-# Add header
-st.header("NBA Database")
+# Add a centered header
+st.markdown(
+    """
+    <h1 style="text-align: center;">NBA Database</h1>
+    """,
+    unsafe_allow_html=True,
+)
