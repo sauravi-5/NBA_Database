cf7ac2ddc68be0fa212769fadffa82c99506dfef
sauravi-5 <92053483+sauravi-5@users.noreply.github.com>
2024-11-16 17:56:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,22 +14,22 @@ st.markdown(
     <div style="border: 2px solid white; padding: 20px; text-align: center; font-size: 20px;">
         Player and Game Statistics
         <div style="display: flex; justify-content: space-around; margin-top: 20px;">
-            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9; display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <h3 style="margin-bottom: 0; color: black;">Stat 1</h3>
                 <hr style="border: 1px solid black; margin-top: 5px; margin-bottom: 10px;">
                 <p style="font-size: 18px; color: black;">100</p>
             </div>
-            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9; display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <h3 style="margin-bottom: 0; color: black;">Stat 2</h3>
                 <hr style="border: 1px solid black; margin-top: 5px; margin-bottom: 10px;">
                 <p style="font-size: 18px; color: black;">200</p>
             </div>
-            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9; display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <h3 style="margin-bottom: 0; color: black;">Stat 3</h3>
                 <hr style="border: 1px solid black; margin-top: 5px; margin-bottom: 10px;">
                 <p style="font-size: 18px; color: black;">300</p>
             </div>
-            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9; display: flex; flex-direction: column; align-items: center; justify-content: center;">
                 <h3 style="margin-bottom: 0; color: black;">Stat 4</h3>
                 <hr style="border: 1px solid black; margin-top: 5px; margin-bottom: 10px;">
                 <p style="font-size: 18px; color: black;">400</p>
