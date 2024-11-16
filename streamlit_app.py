4a76e1ec306ae473afe3b3358fee9de222add36a
sauravi-5 <92053483+sauravi-5@users.noreply.github.com>
2024-11-16 17:52:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,21 +16,25 @@ st.markdown(
         <div style="display: flex; justify-content: space-around; margin-top: 20px;">
             <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3 style="margin-bottom: 0;">Box 1</h3>
+                <p style="margin-bottom: 5px;">Stat 1</p>
                 <hr style="border: 1px solid black; margin-top: 0; margin-bottom: 10px;">
                 <p style="font-size: 18px;">100</p>
             </div>
             <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3 style="margin-bottom: 0;">Box 2</h3>
+                <p style="margin-bottom: 5px;">Stat 2</p>
                 <hr style="border: 1px solid black; margin-top: 0; margin-bottom: 10px;">
                 <p style="font-size: 18px;">200</p>
             </div>
             <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3 style="margin-bottom: 0;">Box 3</h3>
+                <p style="margin-bottom: 5px;">Stat 3</p>
                 <hr style="border: 1px solid black; margin-top: 0; margin-bottom: 10px;">
                 <p style="font-size: 18px;">300</p>
             </div>
             <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3 style="margin-bottom: 0;">Box 4</h3>
+                <p style="margin-bottom: 5px;">Stat 4</p>
                 <hr style="border: 1px solid black; margin-top: 0; margin-bottom: 10px;">
                 <p style="font-size: 18px;">400</p>
             </div>
