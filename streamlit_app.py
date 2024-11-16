b2a45eade91444c601eed14f33e54a3865c402f1
sauravi-5 <92053483+sauravi-5@users.noreply.github.com>
2024-11-16 17:50:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,25 +14,25 @@ st.markdown(
     <div style="border: 2px solid white; padding: 20px; text-align: center; font-size: 20px;">
         Player and Game Statistics
         <div style="display: flex; justify-content: space-around; margin-top: 20px;">
-            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3>Box 1</h3>
-                <hr style="border: 1px solid black; margin: 5px 0;">
-                <p style="font-size: 18px; margin-top: 20px;">100</p>
+                <hr style="border: 1px solid black; margin: 0;">
+                <p style="font-size: 18px; margin-top: 10px;">100</p>
             </div>
-            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3>Box 2</h3>
-                <hr style="border: 1px solid black; margin: 5px 0;">
-                <p style="font-size: 18px; margin-top: 20px;">200</p>
+                <hr style="border: 1px solid black; margin: 0;">
+                <p style="font-size: 18px; margin-top: 10px;">200</p>
             </div>
-            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3>Box 3</h3>
-                <hr style="border: 1px solid black; margin: 5px 0;">
-                <p style="font-size: 18px; margin-top: 20px;">300</p>
+                <hr style="border: 1px solid black; margin: 0;">
+                <p style="font-size: 18px; margin-top: 10px;">300</p>
             </div>
-            <div style="border: 1px solid black; padding: 20px; width: 20%; background-color: #f9f9f9;">
+            <div style="border: 1px solid black; padding: 10px 20px; width: 20%; background-color: #f9f9f9;">
                 <h3>Box 4</h3>
-                <hr style="border: 1px solid black; margin: 5px 0;">
-                <p style="font-size: 18px; margin-top: 20px;">400</p>
+                <hr style="border: 1px solid black; margin: 0;">
+                <p style="font-size: 18px; margin-top: 10px;">400</p>
             </div>
         </div>
     </div>
